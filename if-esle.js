8ef910272d59e87a1e3569bf64e26108ee6dbb3f
Muhammad Owais <muhammadowais98746@gmail.com>
2025-07-16 01:54:19 -0700

    add files

---
+++ if-esle.js
@@ -0,0 +1,8 @@
+// Practice Question # 03
+// Get user to input a number using prompt("Enter a number"). Check if the number is multiple of 5 or not.
+
+let userNum = prompt("Enter any number");
+
+if (userNum % 5) {
+    console.log("User Number is Multiple of 5");
+}
---
+++ simpleObj.js
@@ -0,0 +1,20 @@
+// Practice Question # 01
+// Create a const object called "product" to store information shown in the picture
+const product = {
+    name: "Dollar pen",
+    star: 5,
+    rate: 2000,
+    price: 270
+}
+
+// Practice Question # 02
+// Create a const object called "profile" to store information shown in the picture
+const profile = {
+    posts: 195,
+    followers: 569,
+    following: 4,
+    name: "Muhammad Owais",
+    profile : "@muhammadowaismasroor",
+    intro : "Here you can type your ingtroduction"
+
+}

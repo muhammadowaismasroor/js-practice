60b9bc12e607ce41542071da13610e03d6fa5f1c
Muhammad Owais <muhammadowais98746@gmail.com>
2025-07-28 23:20:21 -0700

    solve question no.6

--- loop.js
+++ loop.js
@@ -11,11 +11,17 @@ for (let i = 0; i <= 100; i++) {
 // Practice Question # 06
 // Create a game where you start with any random game number. Ask the user to 
 // keep guessing the game number until the user enters correct value 
-
 console.log("Question No. 06 Answer");
 
 let guessNumber = 20;
 let userNumber = prompt("Guess any number");
+
+while (userNumber != guessNumber) {
+    userNumber = prompt("guess again...")
+}
+console.log(`your guessing number ${guessNumber} which is correct`);
+
+
 
 
 

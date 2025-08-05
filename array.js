576a7b811021d2baee5f2df6b0cc6f267162f255
Muhammad Owais <muhammadowais98746@gmail.com>
2025-08-04 23:42:02 -0700

    solve question # 09

--- array.js
+++ array.js
@@ -12,3 +12,16 @@ for (let val of stdMarks) {
 
 let avg = sum / stdMarks.length;
 console.log(`the average marks of students is ${avg}`);
+
+
+// Practice Question # 09
+// For a given array with a price of 5 items -> [250, 645, 300, 900, 50]
+// All items have an offer of 10% OFF on them. Change the array to store final price after apply offer.
+console.log("Question No. 09 Answer");
+
+let itemPrice = [250, 645, 300, 900, 50];
+for (let price of itemPrice) {
+    let discount = price * 10 / 100;
+    let finalAmount = price - discount;
+    console.log(`The amount of item before discount is ${price} and after discount is ${finalAmount}`);
+}

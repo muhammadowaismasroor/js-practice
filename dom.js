37d17e0010d1732bedc1d91b852b46a1670fabc1
Muhammad Owais <muhammadowais98746@gmail.com>
2025-08-08 00:33:51 -0700

    solving question no 17

--- dom.js
+++ dom.js
@@ -15,3 +15,20 @@ let divs = document.querySelectorAll("div");
 divs[0].innerText = "owais";
 divs[1].innerText = "ahmed";
 divs[2].innerText = "raziq";
+
+// Practice Question # 17
+//Create a new button element. Give it a text "click me", background color of red & text color of white.
+// a. Insert the button as the first element inside the body tag.
+console.log("Question No. 17 Answer (a)");
+
+let btn = document.createElement("button");
+btn.innerText = "click me";
+btn.style.backgroundColor = "red";
+btn.style.color = "white";
+
+document.querySelector("body");
+console.log(btn);
+
+
+
+

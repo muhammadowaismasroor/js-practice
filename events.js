4639bf33e6c91bf9b17543dc26736269ecec5703
Muhammad Owais <muhammadowais98746@gmail.com>
2025-08-10 01:14:20 -0700

    Solved question no 19

--- events.js
+++ events.js
@@ -1,3 +1,19 @@
 // Practice Question # 19
-
+//Create a toggle button that changes the screen to dark-mode when clicked & light-mode when clicked again
 console.log("Question No. 19 Answer (a)");
+
+let body = document.querySelector("body");
+let btn = document.querySelector("#mode");
+let mode = true;
+
+btn.addEventListener("click", (e) => {
+    if (mode === true) {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        mode = false;
+    } else {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+        mode = true;
+    }
+})
